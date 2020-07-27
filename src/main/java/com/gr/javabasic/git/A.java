960010c305b8a460b35418329e4cafd41fff6bb2
Admin_su <sutawind@163.com>
2020-07-27 15:03:42 +0800

    13

--- src/main/java/com/gr/javabasic/git/A.java
+++ src/main/java/com/gr/javabasic/git/A.java
@@ -10,5 +10,6 @@ public class A {
         System.out.println("github添加");
         System.out.println("github添加2");
         System.out.println("主干添加2");
+        System.out.println("主干添加");
     }
 }
